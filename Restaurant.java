ad4e62aaab2d1275611f5569bc5f1116bc36e8e9
Blen <bryan.lennox@gmail.com>
2012-11-08 23:56:52 -0800

    Changes made to main restaurant class

--- Restaurant.java
+++ Restaurant.java
@@ -1,17 +1,41 @@
-import java.util.Random;
+import java.util.Scanner;
 
 
 public class Restaurant {
 
-
+	Waiter Bob = new Waiter("Bob", (double) 0);
+	Waiter Drewbie = new Waiter("Drewbie", (double) 0);
+	static Menu men = new Menu();
+	
 	
 	public static void main(String[] args) 
 	{
-	
-		System.out.println("please choose menu items:");
+		men.displayMenu();
 		
 		
+		Scanner scan = new Scanner(System.in);
+		String choice = scan.next();
 		
-	}
+		while (!choice.equals("no"))
+		{	if (choice.equals("yes"))
+			{	
+			
+			
+			System.out.println("Which item would you like?"); // Do something
+			int item = scan.nextInt();
+			
+			}
+			else if (choice.equals("view"))
+			{	// Do something
+			}
+			else
+				System.out.println("Invalid input!");
 
-}
+		    men.displayMenu();
+			choice = scan.next();
+		}
+			
+		
+	
+
+}}
