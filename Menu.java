05a32bd8e1337757f78c17d8e6ea80efea1412b2
John Pick <rajohn9@hotmail.com>
2012-11-08 22:31:46 -0800

    Menu to test repo

---
+++ Menu.java
@@ -0,0 +1,21 @@
+
+public class Menu {
+	
+	String[] items = new String[5];
+	double[] price = new double[5];
+	
+	
+	items[0] = 
+	items[1] =
+	items[2] =
+	items[3] =
+	items[4] =
+	
+	price[0] =
+	price[1] =
+    price[2] =
+	price[3] =
+	price[4] =
+	
+
+}
