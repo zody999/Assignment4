0d5e13158adcef05ca0a8c640526f7dfeb39fb39
John Pick <rajohn9@hotmail.com>
2012-11-08 22:48:19 -0800

    Task1 completed menu

--- Menu.java
+++ Menu.java
@@ -1,21 +1,39 @@
 
 public class Menu {
-	
-	String[] items = new String[5];
-	double[] price = new double[5];
-	
-	
-	items[0] = 
-	items[1] =
-	items[2] =
-	items[3] =
-	items[4] =
-	
-	price[0] =
-	price[1] =
-    price[2] =
-	price[3] =
-	price[4] =
-	
 
+	String[] items = new String[5];
+	double[] prices = new double[5];
+	
+	public Menu()
+	{	
+		items[0] = "Beer";
+		items[1] = "Pizza";
+		items[2] = "Burgers";
+		items[3] = "Tacos";
+		items[4] = "Hotdogs";
+		
+		prices[0] = 9.99;
+		prices[1] = 3.50;
+		prices[2] = 5.00;
+		prices[3] = 4.50;
+		prices[4] = 2.50;
+		
+		
+	}
+	
+	
+	public void displayMenu()
+	{
+		System.out.print("Browse the Menu!: ");
+		for (int i = 1; i<6; i++)
+		{
+			System.out.println(i+": "+items[i-1]);
+			System.out.print("  -"+prices[i-1]);
+		}
+		
+		
+	}
+	
+	
+	
 }
