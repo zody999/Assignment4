305b13d2d46fff1f189e47960a69b4c6dd89ec39
John Pick <rajohn9@hotmail.com>
2012-11-08 23:08:08 -0800

    Assigned random waiter

--- Restaurant.java
+++ Restaurant.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 
 public class Restaurant {
 
@@ -7,12 +9,22 @@ public class Restaurant {
 	
 	public static void main(String[] args) 
 	{
+		Random ranWaiter = new Random();
+		int whichW = ranWaiter.nextInt(2);
 		
-	
-		
-		
+		if(whichW == 0)
+		{
+			System.out.println("Hello! I'm Bob and I will be your server today!");
+		}
+		else
+		{
+			System.out.println("Greetings! My name is Drewbie, may I take your order?");
+		}
 		
 		System.out.println("please choose menu items:");
+		
+		
+		
 	}
 
 }
