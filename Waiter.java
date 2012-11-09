9654c67893b331b1a7f89849e18fe9f28d7871b8
Blen <bryan.lennox@gmail.com>
2012-11-08 22:28:11 -0800

    Waiter!

---
+++ Waiter.java
@@ -0,0 +1,13 @@
+
+public class Waiter {
+
+     public String name;
+     public Double tips;
+     
+     
+     public Waiter(String a, Double b) {
+         name = a;
+         tips = b;
+     }
+
+}
