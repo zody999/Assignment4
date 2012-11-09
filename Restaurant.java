8c1752aec032db2fa974f2d11a80434088d4bfa7
Blen <bryan.lennox@gmail.com>
2012-11-08 22:52:45 -0800

    Retaurant class

---
+++ Restaurant.java
@@ -0,0 +1,18 @@
+
+public class Restaurant {
+
+	Waiter Bob = new Waiter("Bob", (double) 0);
+	Waiter Drewbie = new Waiter("Drewbie", (double) 0);
+	
+	
+	public static void main(String[] args) 
+	{
+		
+	
+		
+		
+		
+		System.out.println("please choose menu items:");
+	}
+
+}
